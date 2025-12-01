14606ba499c8eba330503ed81be81a333f7109dc
Krzysztof Debski <kdebski85@gmail.com>
2025-12-01 11:38:07 +0100

    task 1

--- src/main/java/pl/krzysztofdebski/task1/Task1b.java
+++ src/main/java/pl/krzysztofdebski/task1/Task1b.java
@@ -5,23 +5,28 @@ import java.nio.file.Path;
 
 import static java.nio.file.Files.readAllLines;
 
-public class Task1 {
+public class Task1b {
 
     public static void main(String[] args) throws IOException {
         int pos = 50;
         int result = 0;
-       // String input = "src/main/resources/task1/1-sample.input";
         String input = "src/main/resources/task1/1-task.input";
+        // String input = "src/main/resources/task1/1-task.input";
 
         for (String line : readAllLines(Path.of(input))) {
             int r = Integer.parseInt(line.substring(1));
             if (line.startsWith("L")) {
+                result += r / 100;
+                if (pos - r % 100 < 0 && pos > 0) {
+                    result++;
+                }
+                if (r % 100 == pos) {
+                    result++;
+                }
                 pos = (pos - r + 1000) % 100;
             } else {
+                result += (pos + r) / 100;
                 pos = (pos + r) % 100;
-            }
-            if (pos == 0) {
-                result++;
             }
         }
 

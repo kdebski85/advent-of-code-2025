3a824d8a8abd10b469c717ff2d85b935e3518671
Krzysztof Debski <kdebski85@gmail.com>
2025-12-09 16:40:50 +0100

    simplify task 2

--- src/main/java/pl/krzysztofdebski/task2/Task2.java
+++ src/main/java/pl/krzysztofdebski/task2/Task2.java
@@ -5,29 +5,26 @@ import java.nio.file.Path;
 import java.util.List;
 
 import static java.nio.file.Files.readString;
-import static pl.krzysztofdebski.utils.ParsingUtils.longs;
+import static pl.krzysztofdebski.utils.ParsingUtils.unsignedLongs;
 
 public class Task2 {
 
     public static void main(String[] args) throws IOException {
         long result = 0;
-      //  String input = "src/main/resources/task2/2-sample.input";
+        //  String input = "src/main/resources/task2/2-sample.input";
         String input = "src/main/resources/task2/2-task.input";
         String line = readString(Path.of(input));
-        List<Long> ranges = longs(line);
-        for (int index = 0; index < ranges.size(); index+=2) {
+        List<Long> ranges = unsignedLongs(line);
+        for (int index = 0; index < ranges.size(); index += 2) {
             long begin = ranges.get(index);
-            long end = -ranges.get(index + 1);
+            long end = ranges.get(index + 1);
             for (long i = begin; i <= end; i++) {
-                int length = (int) (Math.log10(i) + 1);
-                if (length % 2 == 0) {
-                    long first_n = (long) (i / Math.pow(10, Math.floor(Math.log10(i)) - (double)(length  / 2) + 1));
-                    long l = (long) (first_n *  Math.pow(10, length / 2) + first_n);
-                    if (l == i) {
+                String s = String.valueOf(i);
+                if (s.length() % 2 == 0) {
+                    if (s.substring(0, s.length() / 2).equals(s.substring(s.length() / 2))) {
                         result += i;
                     }
                 }
-
             }
         }
 
--- src/main/java/pl/krzysztofdebski/task2/Task2b.java
+++ src/main/java/pl/krzysztofdebski/task2/Task2b.java
@@ -5,32 +5,36 @@ import java.nio.file.Path;
 import java.util.List;
 
 import static java.nio.file.Files.readString;
-import static pl.krzysztofdebski.utils.ParsingUtils.longs;
+import static pl.krzysztofdebski.utils.ParsingUtils.unsignedLongs;
 
 public class Task2b {
 
     public static void main(String[] args) throws IOException {
         long result = 0;
-       // String input = "src/main/resources/task2/2-sample.input";
+        // String input = "src/main/resources/task2/2-sample.input";
         String input = "src/main/resources/task2/2-task.input";
         String line = readString(Path.of(input));
-        List<Long> ranges = longs(line);
-        for (int index = 0; index < ranges.size(); index+=2) {
+        List<Long> ranges = unsignedLongs(line);
+        for (int index = 0; index < ranges.size(); index += 2) {
             long begin = ranges.get(index);
-            long end = -ranges.get(index + 1);
-            m: for (long i = begin; i <= end; i++) {
-                int length = (int) (Math.log10(i) + 1);
-                for (int j = 1; j <= length / 2; j++) {
-                    if (length % j == 0) {
-                        long first_n = (long) (i / Math.pow(10, Math.floor(Math.log10(i)) - j + 1));
-                        long l = first_n;
-                        for (int k = 1; k < length / j; k++) {
-                            l += (long) (first_n * Math.pow(10, k * j));
+            long end = ranges.get(index + 1);
+
+            rangeCheck:
+            for (long i = begin; i <= end; i++) {
+                String numberAsString = String.valueOf(i);
+                int length = numberAsString.length();
+
+                substringCheck:
+                for (int substringLength = 1; substringLength <= length / 2; substringLength++) {
+                    if (length % substringLength == 0) {
+                        String firstSubstring = numberAsString.substring(0, substringLength);
+                        for (int k = 1; k < length / substringLength; k++) {
+                            if (!firstSubstring.equals(numberAsString.substring(k * substringLength, k * substringLength + substringLength))) {
+                                continue substringCheck;
+                            }
                         }
-                        if (l == i) {
-                            result += i;
-                            continue m;
-                        }
+                        result += i;
+                        continue rangeCheck;
                     }
                 }
             }
